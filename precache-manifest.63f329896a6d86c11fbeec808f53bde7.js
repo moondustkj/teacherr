54d516dae5aebfac4a35b27cb7769563d6e13ba2
komaljituri <komaljituri88@gmail.com>
2019-12-16 14:53:51 +0530

    Updates

---
+++ precache-manifest.63f329896a6d86c11fbeec808f53bde7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e85e96e676c6ea7febf3d41a2774ecf",
+    "url": "/teacherr/index.html"
+  },
+  {
+    "revision": "11ff6390f467a8016a79",
+    "url": "/teacherr/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c32fbade975f2290fe5c",
+    "url": "/teacherr/static/css/main.ee6e633f.chunk.css"
+  },
+  {
+    "revision": "11ff6390f467a8016a79",
+    "url": "/teacherr/static/js/2.74f79252.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/teacherr/static/js/2.74f79252.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c32fbade975f2290fe5c",
+    "url": "/teacherr/static/js/main.7bc9ceda.chunk.js"
+  },
+  {
+    "revision": "522369ec19661a7862e4",
+    "url": "/teacherr/static/js/runtime-main.073e60ee.js"
+  }
+]);
